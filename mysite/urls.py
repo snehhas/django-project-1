4cc55d819dcf1852eb3117327e5e5be22928a4e4
Fadhil Yori Hibatullah <fadhilyori12@outlook.com>
2018-06-17 01:36:33 +0700

    Add: login

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # url(r'^login/', auth_views.login, name='login'),
+    url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^admin/', admin.site.urls),
 ]
