2c608f03018d9d19fc50005f7d15ce0968b1184d
Sneha Sunny <snehasunny321@gmail.com>
2024-07-06 00:23:05 +0530

    Book Inventory

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('books/', include('books.urls')),
 ]
