54dde8ce711538d5b73396e8ba893c020aa023e3
Sneha Sunny <snehasunny321@gmail.com>
2024-07-06 16:10:59 +0530

    Book loan changes

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django import forms
+from django.contrib.auth.models import User
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
@@ -12,3 +13,12 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date', 'isbn']
+
+class BorrowedBook(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrowed_date = models.DateTimeField(auto_now_add=True)
+    returned_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} borrowed {self.book.title}"
--- books/urls.py
+++ books/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.book_list, name='book_list'),
     path('add/', views.book_add, name='book_add'),
     path('<int:pk>/edit/', views.book_edit, name='book_edit'),
+    path('borrow/<int:book_id>/', views.borrow_book, name='borrow_book'),
     # Add more URLs as needed
 ]
