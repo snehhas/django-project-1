1e26b1ce08b443f8d46ac88933a858d42356463a
Sneha Sunny <snehasunny321@gmail.com>
2024-07-07 19:33:09 +0530

    Login authentication for pages

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
 from .models import Member
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -16,6 +18,7 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
 
+@login_required
 def user_list(request):
     users = User.objects.all()
     return render(request, 'accounts/user_list.html', {'users': users})
